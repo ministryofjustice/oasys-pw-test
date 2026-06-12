635c5c1f094c76b409d50d5ac2bbd71b074aa361
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-06-12 15:19:55 +0100

    First cut of TypeDoc documentation

--- fixtures/api/api.ts
+++ fixtures/api/api.ts
@@ -8,18 +8,6 @@ import * as checkApiResponse from './checkApiResponse'
 import * as restApiDb from './data/restApiDb'
 
 
-/** 
- * Tests all endpoints for all assessments for given offender probation CRN; uses a result alias to return a true/false result (true if any tests fail).
- * All necessary data is pulled from the database to build expected responses.
- * 
- * Parameters:
- *  - crn (either probation or prison CRN as specified by the next parameter)
- *  - crnSource - 'prob' or 'pris'
- *  - skipPkOnlyCalls - if true, any APIs that are called with just an assessment PK will be skipped on the basis that the calling script is repeating an offender 
- *                      (selected this time using the prison CRN instead of probation) so these calls will be identical.
- *  - stats - optional array of EndpointStat objects to collect timing stats
- */
-
 export class Api {
 
     constructor(private readonly oasysDb: OasysDb, private readonly request: APIRequestContext) { }

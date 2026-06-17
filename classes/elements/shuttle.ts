d1721aa3eb5197af735bfe0b4e52750edef65058
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-06-17 11:44:22 +0100

    Added missing shuttle functions

--- classes/elements/shuttle.ts
+++ classes/elements/shuttle.ts
@@ -15,7 +15,9 @@ export class Shuttle {
     async addItems(items: string[]) {
 
         for (const item of items) {
-            await this.addItemUsingFilter(item)
+
+            await this.goToStart('left')
+            await this.findAndSelect('left', item)
         }
 
         log(`Added items to ${this.selector}: ${JSON.stringify(items)}`)
@@ -39,4 +41,55 @@ export class Shuttle {
         await this.page.locator(`#select${titleCaseSide}${this.selector}`).locator('option').nth(index).click()
     }
 
+    async getItems(side: 'left' | 'right'): Promise<string[]> {
+
+        const titleCaseSide = side.charAt(0).toUpperCase() + side.substring(1)
+        const options = await this.page.locator(`#select${titleCaseSide}${this.selector}`).locator('option').allTextContents()
+        return options
+    }
+
+    async goToStart(side: 'left' | 'right') {
+
+        await this.clickUntilGone(side, 'prev')
+    }
+
+    async clickUntilGone(side: 'left' | 'right', button: 'prev' | 'next', attempt = 0) {
+
+        if (attempt === 20) {
+            throw new Error(`clickUntilGone failure for ${side}`)
+        }
+
+        const titleCaseSide = side.charAt(0).toUpperCase() + side.substring(1)
+        const buttonSelector = `#${button}Page${titleCaseSide}${this.selector}`
+
+        const enabledCount = await this.page.locator(`#shuttle${this.selector}`).locator(`${buttonSelector}:enabled`).count()
+        if (enabledCount > 0) {
+            await this.page.locator(buttonSelector).click()
+            await this.clickUntilGone(side, button, ++attempt)
+        }
+    }
+
+    async findAndSelect(side: 'left' | 'right', item: string, attempt = 0) {
+
+        if (attempt === 20) {
+            throw new Error(`findAndSelect failure for ${item}`)
+        }
+
+        const titleCaseSide = side.charAt(0).toUpperCase() + side.substring(1)
+        const buttonSelector = `#nextPage${titleCaseSide}${this.selector}`
+
+        const items = await this.getItems(side)
+        if (items.includes(item)) {
+            await this.page.locator(`#select${titleCaseSide}${this.selector}`).locator(`option:has-text('${item}')`).click()
+            await this.clickButton('select')
+        } else {
+
+            const enabledCount = await this.page.locator(`#shuttle${this.selector}`).locator(`${buttonSelector}:enabled`).count()
+            if (enabledCount > 0) {
+                await this.page.locator(buttonSelector).click()
+                await this.findAndSelect(side, item, ++attempt)
+            }
+        }
+
+    }
 }

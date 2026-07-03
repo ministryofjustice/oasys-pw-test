4e07436edebbee0b9eda7909c7e1003001d1f16a
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-07-03 15:35:22 +0100

    Fixed error reporting

--- tests/api/testAllApisMultipleOffenders.test.ts
+++ tests/api/testAllApisMultipleOffenders.test.ts
@@ -18,7 +18,7 @@ const dateConditions = [
 ]
 
 const limitEndpoints: Endpoint[] = []
-// const limitEndpoints: Endpoint[] = ['crimNeeds']
+//const limitEndpoints: Endpoint[] = ['pni']
 
 const excludeEndpoints: Endpoint[] = []
 // const excludeEndpoints: Endpoint[] = ['pni']
@@ -60,8 +60,8 @@ for (let i = 0; i < dateConditions.length; i++) {
                 where rownum <= ${dateConditions[i].count}`
 
         const offenders = await oasysDb.getData(offenderQuery)
-        await runTest(offenders, api)
-
+        const failed = await runTest(offenders, api)
+        expect(failed).toBeFalsy()
     })
 }
 

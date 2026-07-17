c921b3322f54260a6554c7185a4c0df03c0890ff
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-07-17 15:15:48 +0100

    Removed some temporary changes

--- tests/examples/createProbationOffenderAndSAN.test.ts
+++ tests/examples/createProbationOffenderAndSAN.test.ts
@@ -5,14 +5,14 @@ test('Example test - create a prison offender and a SAN 3.2 assessment', async (
 
     await user.prob.probSanHeadPdu.login()
 
-    const offender1 = await offender.createProbFromStandardOffender({type: 'sexual'})
-    // await assessment.createProb({ purposeOfAssessment: 'Start of Community Order', assessmentLayer: 'Full (Layer 3)', includeSanSections: 'Yes' })
+    const offender1 = await offender.createProbFromStandardOffender()
+    await assessment.createProb({ purposeOfAssessment: 'Start of Community Order', assessmentLayer: 'Full (Layer 3)', includeSanSections: 'Yes' })
 
-    // // Use one of the following two lines to populate the assessment.  maxStrings paramater can be set to populate text fields to maximum length
-    // await assessment.populateMinimal({ layer: 'Layer 3V2' })
-    // // await oasys.Populate.fullyPopulated({layer: 'Layer 1',provider: 'pris',  sentencePlan: 'SpService' , maxStrings: false })
+    // Use one of the following two lines to populate the assessment.  maxStrings paramater can be set to populate text fields to maximum length
+    await assessment.populateMinimal({ layer: 'Layer 3V2' })
+    // await oasys.Populate.fullyPopulated({layer: 'Layer 1',provider: 'pris',  sentencePlan: 'SpService' , maxStrings: false })
 
-    // await signing.signAndLock()
+    await signing.signAndLock()
 
     await user.logout()
 

b747549594edf3229e98df6e3a8e0857ae21bac8
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-07-23 14:20:26 +0100

    Updated preprod test cases

--- tests/api/testAPIsSpecificOffenders.test.ts
+++ tests/api/testAPIsSpecificOffenders.test.ts
@@ -14,20 +14,22 @@ const testCases = [
     ['ZWMCLZB', null],      // Obscure PNI defect (NOD-1284)
 
     // Pre-prod cases for PNI
-    // ['V217229', ''],
-    // ['A9029EY', ''],
-    // ['E591983', ''],
-    // ['E445066', ''],
-    // ['E475550', ''],
-    // ['T027274', ''],
-    // ['E496953', ''],
-    // ['E050428', ''],
-    // ['E506857', ''],
-    // ['E599091', ''],
-    // ['E716844', ''],
-    // ['E679100', ''],  // This one is still an issue in release 7.9
-    // ['S016052', ''],
-    // ['A2144FH', ''],
+    // ['V217229', null],
+    // ['E445066', null],
+    // ['E475550', null],
+    // ['T027274', null],
+    // ['E496953', null],
+    // ['E050428', null],
+    // ['E506857', null],
+    // ['E599091', null],
+    // ['E716844', null],
+    // ['E679100', null],          // NOD-1284
+    // [null, 'A9029EY'],          //  (case raised by John for NOD-1196)
+    // ['E591983', null],          //  - 1.30 not reported missing
+    // ['S016052', null],          //  - 1.30 not reported missing
+    // [null, 'A2144FH'],          //  - 1.30 not reported missing
+    // ['E776521', null],          //  OGP2 status defect NOD-1313
+    // ['R414385', null],
 ]
 
 const limitEndpoints: Endpoint[] = []
@@ -38,7 +40,7 @@ const excludeEndpoints: Endpoint[] = []
 
 test('All endpoint regression tests - extra test for specific cases', async ({ api }) => {
 
-    let failed = false
+    let failed = 0
     let count = 1
 
     for (const offender of testCases) {
@@ -48,17 +50,17 @@ test('All endpoint regression tests - extra test for specific cases', async ({ a
             const offenderFailed = await api.testOneOffender(offender[0], 'prob', false, true, limitEndpoints, excludeEndpoints)
             if (offenderFailed) {
                 console.log('Failed')
-                failed = true
+                failed++
             }
         }
         if (offender[1] != null) {  // call with NomisId
             const offenderFailed = await api.testOneOffender(offender[1], 'pris', offender[0] != null, true, limitEndpoints, excludeEndpoints)  // skipPrisSubsequents if already done for prob crn
             if (offenderFailed) {
                 console.log('Failed')
-                failed = true
+                failed++
             }
         }
     }
 
-    expect(failed).toBeFalsy()
+    expect(failed).toBe(0)
 })
